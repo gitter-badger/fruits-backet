f0154c2b1501b1501a173136214ea2477ca72a28
Brice Boutillon <boutillon@gmail.com>
2016-01-14 23:01:24 +0100

    Now using yam, also serving as https with express

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 
-var express = require('express'),
-	yaml = require('js-yaml'),
-	fs = require('fs'),
+const express = require('express'),
 	bunyan = require('bunyan'),
-	turnip = require('turnip');
+	https = require('https'),
+	helmet = require('helmet'),
+	turnip = require('turnip'),
+	fs = require('fs'),
+	yam = require('yam');
 
 var app = express(),
 	log = bunyan.createLogger({
@@ -19,16 +21,27 @@ var app = express(),
 
 log.info('Starting');
 
-try {
-	var config = yaml.safeLoad(fs.readFileSync('config.yml', 'utf8'));
-}
-catch (e) {
-	log.fatal('Couldn\'t read config.yml !!! Dying... painfully.');
+var settings = yam(__dirname);
+
+if (settings.err) {
+	for (var i in settings.err) log.fatal(settings.err[i]);
+	log.fatal('Dying... painfully.');
 	process.exit();
 }
 
-if(config.MAIN && config.MAIN.debug) log.addStream({level: 'debug', type: 'raw', stream: turnip});
+if(settings.config.MAIN.debug) log.addStream({level: 'debug', type: 'raw', stream: turnip});
 
 log.debug('DEBUG ON');
 
-log.addStream({path: (config.MAIN && config.MAIN.log) ? config.MAIN.log : 'fruits-basket.log'});
+log.addStream({path: settings.config.MAIN.log});
+
+app.use(helmet())
+	.use('/public', express.static('public'));
+
+const options = {
+	key: fs.readFileSync(settings.config.HTTPS.key),
+	passphrase: settings.config.HTTPS.pass,
+  	cert: fs.readFileSync(settings.config.HTTPS.cert)
+}
+
+https.createServer(options, app).listen(settings.config.HTTPS.port);
